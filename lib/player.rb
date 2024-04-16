46df511acac888d779b3c7577c890e3fd1c2e34b
1110lias <elias-b@outlook.fr>
2024-04-16 17:58:41 +0200

    Exercice 2.3 terminé

--- lib/player.rb
+++ lib/player.rb
@@ -2,11 +2,17 @@ require 'pry'
 
 class Player
     attr_accessor :user_name, :life_points
-
+    @@enemies = []
+    
     def initialize(name) # is executed for each new instance in "Player class"
         @user_name = name
         @life_points = 10
+        @@enemies << self
     end
+
+    # def self.all_enemies
+    #     return @@enemies
+    # end
 
     def show_state # gives the life_points status of a given user.
         puts "#{@user_name} a #{@life_points} points de vie restants\n"
@@ -27,7 +33,53 @@ class Player
     end
 
     def compute_damage
-        return rand(1..6)        
+        return rand(100..600)        
+    end
+end
+
+class HumanPlayer < Player
+    attr_accessor :weapon_level, :user_name 
+
+    def initialize(name)
+        @life_points = 100
+        @user_name = name
+        @weapon_level = 1
     end
 
+    def show_state # gives the life_points status of a given user.
+        puts "#{@user_name} a #{@life_points} points de vie restants et une arme de niveau #{@weapon_level}.\n"
+    end
+
+    def compute_damage
+        rand(1..6) * @weapon_level
+    end
+    
+    def search_weapon
+        lvl_wp = rand(1..6)
+        puts "AMAZING! Tu as trouvé une arme de niveau #{lvl_wp}!"
+            if lvl_wp > @weapon_level
+                @weapon_level = lvl_wp
+                puts "Cette arme est plus puissante tu la garde"
+            else 
+                puts "Cette arme n'a aucun intéret."
+            end
+        return lvl_wp
+    end
+
+    def search_health_pack
+        draw = rand(1..6)
+        if draw == 1
+            puts "Tu n'as rien trouvé Morray... "
+        elsif draw >= 2 && draw <= 5
+            @life_points += 50
+            puts "Tu viens de trouver de la potion magique, +50 points"
+        else
+        @life_points += 80
+            puts "WTF, tu gagnes +80 points"
+            if (life_points >= 100)
+                @life_points = 100
+                puts "Cependant, tu ne peux pas aller plus haut que ton verre, ta jauge reste a 100 points"
+            end
+        end
+    end           
 end
