2ca8c9cf605b004fe0b425a537a6eb3223b916eb
1110lias <elias-b@outlook.fr>
2024-04-16 15:40:27 +0200

    Exercice 2.2 terminé

--- lib/player.rb
+++ lib/player.rb
@@ -5,32 +5,29 @@ class Player
 
     def initialize(name) # is executed for each new instance in "Player class"
         @user_name = name
-        @life_points = 10                 
+        @life_points = 10
     end
 
     def show_state # gives the life_points status of a given user.
-        puts "#{@user_name} a #{@life_points} points de vie restants\n\n"
+        puts "#{@user_name} a #{@life_points} points de vie restants\n"
     end
 
-    def get_damages(damages, defender) # when player recieves damage from attacks
+    def get_damages(damages) # when player recieves damage from attacks
         @life_points -= damages
-        puts "#{defender.user_name} a perdu #{damages} points de vie et n'a plus que #{@life_points} points de vie"
+        puts "\e[31mIl lui inflige #{damages} points de dégâts\e[0m."
         if @life_points <= 0
-            puts "#{defender.user_name} est mort"
-            #puts "#{attacker.user_name} a gagné!"
+            @life_points = 0
         end
         return damages
     end
 
-    def attack(defender)
+    def attack(defender) 
         puts "\n\e[33mLe joueur #{@user_name} attaque le joueur #{defender.user_name}\e[0m"
-        dice = get_damages(compute_damage, defender)
-        puts "\e[31mIl lui inflige #{dice} points de dégâts\e[0m \n\n"
+        defender.get_damages(compute_damage)
     end
 
     def compute_damage
-    return rand(1..6)        
+        return rand(1..6)        
     end
 
 end
-  
